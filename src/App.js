f0beaa9386aa5d1ae8b1e53e205f1a0d05cad0c8
Syed Umar <khan.afrin02@gmail.com>
2023-08-31 12:13:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { Car } from './component/Car';
+import { Carr } from './component/Carr';
+function App() {
+  return (
+    <div>
+      <Car/>
+      <Carr/>
+      <Car/>
+      <Carr/>
+      <Carr/>
+    </div>
+
+    
+  );
+}
+
+export default App;
---
+++ src/component/Car.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export const Car=()=>{
+    return(
+        <div>
+       <h3>Hyundai i20 sports </h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, atque architecto. Ipsa, nam? Expedita nesciunt consequuntur nisi, doloremque officiis laborum veritatis sint dolorum dolor, odit modi, ullam quaerat dicta pariatur itaque quod voluptate ipsum eius eligendi ut in. Placeat dolores, tempore similique, animi id mollitia unde nobis sequi, odio aut hic non est libero perferendis? Repellendus ipsum voluptates tenetur atque libero id minus perspiciatis, dolores et? Architecto eaque laudantium laborum ducimus animi totam eveniet explicabo fuga quis omnis, nihil eligendi eos nostrum necessitatibus aut qui quas? Commodi necessitatibus quia hic esse. Quas nam explicabo at atque officia. Tempore, necessitatibus amet!</p>
+       
+       </div>
+    )
+}
